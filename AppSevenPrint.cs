0a2141c01882470cc5d122f2a5e0da20b5657cf0
Johnson Dubula <jdubula@iqbusiness.net>
2021-12-06 20:58:00 +0200

    AppSeven

---
+++ AppSevenPrint.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AppSevenFunction
+{
+    public static class AppSevenPrint
+    {
+        [FunctionName("AppSevenPrint")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            return new OkObjectResult("Jam");
+        }
+    }
+}
